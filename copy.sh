8b49b3bb17fb264179e4d07741d13d5ab9d92211
Jasper Andrew <jqaxyz@gmail.com>
2025-08-23 11:55:11 -0400

    update

--- copy.sh
+++ copy.sh
@@ -22,12 +22,42 @@ filepaths+=('.mozilla/firefox/s8csmeui.default-release/chrome/userChrome.css')
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo Clearing old files...
-rm -r $SCRIPT_DIR/files/* 2> /dev/null
+help () {
+   echo "help"
+}
 
-echo Copying from $HOME...
-for f in "${filepaths[@]}"; do
-   echo $HOME/$f "-->" $SCRIPT_DIR/files/$f
-   mkdir -p $SCRIPT_DIR/files/$(dirname $f);
-   cp $HOME/$f $SCRIPT_DIR/files/$f
-done
+docopy () {
+   # echo Clearing old files...
+   # rm -r $SCRIPT_DIR/files/* 2> /dev/null
+
+   echo Copying from $SRC to $DST...
+   for f in "${filepaths[@]}"; do
+      echo $SRC/$f "-->" $DST/$f
+      mkdir -p $DST/$(dirname $f);
+      rm $DST/$f
+      cp $SRC/$f $DST/$f
+   done
+}
+
+up () {
+   SRC=$HOME
+   DST=$SCRIPT_DIR/files
+   docopy
+}
+
+down () {
+   SRC=$SCRIPT_DIR/files
+   DST=$HOME
+   docopy
+}
+
+if [[ $# -eq 0 ]]; then echo "[Error] Argument required"; help
+elif [[ $# -eq 1 ]]; then
+
+	if [[ $1 == "up" ]]; then up
+	elif [[ $1 == "down" ]]; then down
+	else echo "[Error] Invalid argument"; help
+	fi
+
+else echo "[Error] Too many arguments"; help
+fi
